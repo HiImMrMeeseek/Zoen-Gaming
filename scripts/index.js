2e594796bb33f5026b4c1fcfe573a5b09032f575
Enzo Holgado <89498144+HiImMrMeeseek@users.noreply.github.com>
2022-04-08 19:40:48 -0500

    index responsive

--- scripts/index.js
+++ scripts/index.js
@@ -2,25 +2,25 @@ const patrocinadores = [
     {
         title: 'Asus ROG',
         info: 'Asus ROG nos apoya desde los inicios, por eso es nuestro patrocinador principal y los queremos muchoMarca 1 nos apoya desde los inicios, por eso es nuestro patrocinador principal y los queremos muchoMarca 1 nos apoya desde los inicios, por eso es nuestro patrocinador principal y los queremos mucho',
-        web: 'marcas.html',
+        web: 'https://rog.asus.com/co/',
         img: '../images/marca1.png'
     },
     {
         title: 'Redragon',
         info: 'Redragon siempre han sido muy cordiales y transparentes con nosotrxs por eso estamos totalmente agradecidosMarca 2 siempre han sido muy cordiales y transparentes con nosotrxs por eso estamos totalmente agradecidosMarca 2 siempre han sido muy cordiales y',
-        web: 'marcas.html',
+        web: 'https://redragon.es/',
         img: '../images/marca2.png'
     },
     {
         title: 'AORUS',
         info: 'AORUS todo mas que bien con ellxs, en la buena siempre. gracias por los perifericosMarca 3 todo mas que bien con ellxs, en la buena siempre. gracias por los perifericosMarca 3 todo mas que bien con ellxs, en la buena siempre. gracias por los perifericos',
-        web: 'marcas.html',
+        web: 'https://www.aorus.com/es-co/',
         img: '../images/marca3.png'
     },
     {
         title: 'GG.BET',
         info: 'GG.BET es el patrocinador mas nuevo pero no por eso menos que los demas, desde el dia 1 nos dieron todo lo que pudieron y mas.Marca 4 es el patrocinador mas nuevo pero no por eso menos que los demas, desde el dia 1 nos dieron todo lo que pudieron y mas.',
-        web: 'marcas.html',
+        web: 'https://betplay.com.co/',
         img: '../images/marca4.png'
     }
 ]
@@ -54,3 +54,12 @@ closeModal.addEventListener('click', function (e) {
 // foot
 const date = document.getElementById('date');
 date.innerHTML = new Date().getFullYear();
+
+// MOBILE logic
+const showLinks = document.querySelector('.toggle-links');
+const toggleBtn = document.querySelector('.toggle-btn');
+
+toggleBtn.addEventListener('click', function (e) {
+    showLinks.classList.toggle('show-links');
+    toggleBtn.classList.toggle('show-toggle-btn');
+})
